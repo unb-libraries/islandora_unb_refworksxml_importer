b5f18b9111cae809c787bc2cd0aa22a14f38148f
Jacob Sanford <jsanford@unb.ca>
2015-04-15 14:25:50 -0300

    Remove inherited, unchanged method

--- inc/UNBRefworksXMLImporter.inc.php
+++ inc/UNBRefworksXMLImporter.inc.php
@@ -85,26 +85,4 @@ class UNBRefworksXMLImporter extends RefworksXMLImporter {
     );
     return $info;
   }
-
-  /**
-   * Inherited.
-   */
-  public function preprocess() {
-    $preprocessed = array();
-    $item_class = $this->itemClass;
-
-    $total = $this->getNumber();
-    for ($i = 0; $i < $total; $i++) {
-      $item = $item_class::getOne($this->file);
-      if ($item) {
-        $this->parameters['namespace'] = $this->getNamespace($item);
-        $wrapper = $item->getWrapperClass();
-        $preprocessed[] = $object = new $wrapper($this->connection, $item, $this->parameters);
-        $object->addRelationships();
-        $this->addToDatabase($object, $object->getResources());
-      }
-    }
-
-    return $preprocessed;
-  }
 }
